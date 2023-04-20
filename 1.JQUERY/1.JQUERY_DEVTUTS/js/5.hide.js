4328e92b5405218ae00968a70f9e2c9775eb3513
sumitkr0433 <sumitkr0433@gmail.com>
2023-04-20 22:16:19 +0530

    now latest repo maintianing 20-04-2023

---
+++ 1.JQUERY/1.JQUERY_DEVTUTS/js/5.hide.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function(){
+			 $('h1#first').hide('9000');
+			  
+			 
+            });
+	
--- 1.jquery_devtuts/js/5.hide.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){
-			 $('h1#first').hide('9000');
-			  
-			 
-            });
-	
